2f983a5ecd932fd7b2c075bd09a22d9ae46add25
Prateek Gupta <prateekjpg@gmail.com>
2019-01-29 13:53:20 +0530

    Added solution to CHFIDEAL problem

---
+++ DEC18B/CHFIDEAL/chfideal.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  ll x;
+
+  cout<<1<<endl;
+  cin>>x;
+
+  cout<<(x ^ 1)<<endl;
+
+  return 0;
+}
