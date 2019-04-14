f1b8d8d5c0fd51295a0dfdc1f36cdfd2a4fd7d7c
Prateek Gupta <prateekjpg@gmail.com>
2019-04-14 22:01:35 +0530

    Added solution to SIMPSTAT problem

---
+++ LTIME34/SIMPSTAT/simpstat.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define boost ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ff first
+#define ss second
+#define sqr(a) ((a) * (a))
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+
+// "If you can't do something, get it done by someone else ;-) "
+// GCD - __gcd(n1, n2)
+
+// Count no. of occurences - count(s.begin(), s.end(), 'e')
+
+/* Permutations - sort(s.begin(), s.end())
+				  do{
+				  	cout<<s<<endl;
+				  }while(next_permutation(s.begin(), s.end()));
+*/
+// Revese sort - sort(arr, arr+n, greater<ll>())
+
+// Finding substring in another string - 
+// size_t = str.find("geeks.practice", 0, 5); only search first 5 characters from geeks.pra ...
+// str.find("geeks"); search geeks in str
+// str.find("geeks", index+1); find geeks in original string after (index+1) index
+// in str
+// if size_t != string::npos
+
+// Sum of a number in string format eg. "124" 
+// int sum = (accumulate(begin(s), end(s), 0) - '0' * n);
+
+// int sum = 0;
+// sum = accumulate(a, a+n, 0);
+// where 0 is the initial value of sum variable
+
+// s.erase(1) - erase all characters except last
+// s.erase(1,4) - erase 4 characters from index number 1
+
+// cin.ignore(); - compulsory
+// getline(cin, s); - inputs complete string s
+
+//memset(a, 0, sizeof(a)); - initializes array 'a' with all values = 0
+
+using namespace std;
+
+int main()
+{
+	boost;
+
+	// #ifndef ONLINE_JUDGE
+ //            freopen("input.txt", "rt", stdin);
+ //            freopen("output.txt", "wt", stdout);
+ //    #endif
+
+    int T;
+    cin>>T;
+    for (int t = 0 ; t < T ; ++t) {
+        int N, K;
+        cin>>N>>K;
+        vector<int> arr(N);
+        for (int i = 0 ; i < N ; ++i) {
+            cin>>arr[i];
+        }
+        sort(arr.begin(), arr.end());
+        double result = 0;
+        for (int i = 0 ; i < N ; ++i) {
+            if (i >= K && i < N - K) {
+                result += arr[i];
+            }
+        }
+        result /= N - 2 * K;
+        cout<<fixed << showpoint<<setprecision(6)<<setfill('0')<<result<<endl;
+    }
+
+    return 0;
+}
